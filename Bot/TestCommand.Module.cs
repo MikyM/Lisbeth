8dc8049512b726748c5f07b949a002a5f5829f85
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-08-06 18:32:38 +0200

    Main project architecture

---
+++ Bot/TestCommand.Module.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Bot
+{
+    public class TestCommandModule : BaseCommandModule
+    {
+        public Random Rng { private get; set; }
+
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx, int min, int max)
+        {
+            await ctx.RespondAsync($"I'm fukin alive, {Rng.Next(min, max)}");
+        }
+    }
+}
