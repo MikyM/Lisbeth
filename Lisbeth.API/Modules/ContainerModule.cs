b787df65cf72d79418cf6b8a5a588f6c10549265
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-08-15 18:24:09 +0200

    fix: module comms

--- Lisbeth.API/Modules/ContainerModule.cs
+++ Lisbeth.API/Modules/ContainerModule.cs
@@ -9,6 +9,7 @@ using MikyM.Common.Application.Interfaces;
 using MikyM.Common.Application.Services;
 using MikyM.Common.DataAccessLayer.UnitOfWork;
 using System.Reflection;
+using MikyM.Common.DataAccessLayer.Repositories;
 using Module = Autofac.Module;
 
 namespace Lisbeth.API.Modules
@@ -18,15 +19,24 @@ namespace Lisbeth.API.Modules
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
-            // stuff
+            // automapper
             builder.RegisterAutoMapper(opt => opt.AddExpressionMapping(), Assembly.GetExecutingAssembly());
+            // unitofwork
             builder.RegisterGeneric(typeof(UnitOfWork<>)).As(typeof(IUnitOfWork<>)).InstancePerLifetimeScope();
+            // generic services
             builder.RegisterGeneric(typeof(ReadOnlyService<,,>)).As(typeof(IReadOnlyService<,>))
                 .InstancePerLifetimeScope();
             builder.RegisterGeneric(typeof(CrudService<,,>)).As(typeof(ICrudService<,>))
                 .InstancePerLifetimeScope();
+            // generic repositories
+            builder.RegisterGeneric(typeof(ReadOnlyRepository<>)).As(typeof(IReadOnlyRepository<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>))
+                .InstancePerLifetimeScope();
+            // bulk register custom services - follow naming convention
             builder.RegisterAssemblyTypes(typeof(AuditLogService).Assembly).Where(t => t.Name.EndsWith("Service"))
                 .AsImplementedInterfaces().InstancePerLifetimeScope();
+            // bulk register custom repositories - follow naming convention
             builder.RegisterAssemblyTypes(typeof(AuditRepository).Assembly).Where(t => t.Name.EndsWith("Repository"))
                 .AsImplementedInterfaces().InstancePerLifetimeScope();
 
